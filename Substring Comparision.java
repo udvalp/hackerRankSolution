610175048d414fddcd32d1988c442a2d0e2eb65f
udvalp <udval.ce@gmail.com>
2017-10-12 16:41:35 -0700

    Create Substring Comparision.java

---
+++ Substring Comparision.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+  
+    public static String getSmallestAndLargest(String s, int k) {
+        String smallest = "";
+        String largest = "";
+        //ArrayList A = new ArrayList();
+      
+        SortedSet<String> abc = new TreeSet<String>();
+        for(int i=0;i<s.length()-(k-1);i++){
+            
+            abc.add(s.substring(i,i+k));
+        }
+        smallest =abc.first();
+        largest = abc.last();
+        
+        // Complete the function
+        // 'smallest' must be the lexicographically smallest substring of length 'k'
+        // 'largest' must be the lexicographically largest substring of length 'k'
+        
+        return smallest + "\n" + largest;
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        int k = scan.nextInt();
+        scan.close();
+      
+        System.out.println(getSmallestAndLargest(s, k));
+    }
+}
